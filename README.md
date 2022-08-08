bc90981efa0c6c61836ec37c4abfb24803bc3272
Sandesh Katkaam <sandeshkatakam@gmail.com>
2022-08-08 12:56:18 +0530

    Created the project with first line of sample code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Numerical-Analysis-Library
 This repository contains the code implementation of Numerical Analysis Concepts in C++
+
+Numerical Analysis Algorithms available in this library:
+* 
---
+++ main.cpp
@@ -0,0 +1,23 @@
+//// *********************************************** ////
+
+// Numerical Analysis Library in C++
+// Date Started: 08-08-2022
+// Author: Sandesh Katakam
+
+//// *********************************************** ////
+
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <alg.h>
+
+using namespace std;
+
+int main(){
+
+
+    printf("The first line of code today")
+    cout << "The first line of code with iostream library" << endl;
+
+    return 0;
+}
